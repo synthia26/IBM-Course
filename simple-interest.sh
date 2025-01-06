cfc8358cd27d707c0b70e4955c854e1763e85381
synthia26 <synthia.mist@gmail.com>
2025-01-06 11:51:20 +0930

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Author: Synthia Islam (synthia26)
+# Input:
+# principal, principal amount
+# rate, annual rate of interest
+# time, time period in years
+
 # Prompt the user for giving input of principal amount, rate, and time
 echo "Enter the principal amount:"
 read principal
 
-echo "Enter the annual interest rate (as a decimal):"
+echo "Enter interest rate per year (as a decimal):"
 read rate
 
-echo "Enter the time period (in years):"
+echo "Enter the time period in years:"
 read time
 
 # Calculate simple interest
